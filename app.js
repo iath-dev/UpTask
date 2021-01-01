00b2378438e25f06fbbe29da669b2da67a665d9c
iath-dev <developer.iath@gmail.com>
2021-01-01 01:54:26 -0500

    Creando aplicación con express

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo')
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+// Creando aplicación de express
+const app = express()
+
+// Aplicando el cors a la aplicación
+app.use(cors())
+
+// Definiendo ruta del home
+app.use('/', (req, res) => {
+    res.status(200).send('Hola mundo')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('====================================');
+    console.log('Servidor iniciado en el puerto', process.env.PORT);
+    console.log('====================================');
+})
